fc280c356c03f7476ebdbbca1633701744277464
opilane <opilane@HP-CZC2349HTN>
2024-01-24 13:22:50 +0200

    Add project files.

---
+++ Models/Birthday.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Party.Models
+{
+    public class Birthday
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Sissesta pidu siia")]
+        public string Nimetus { get; set; }
+
+        [Required(ErrorMessage = "Sissesta paäev siia")]
+        public DateTime Date { get; set; }
+       
+        public string Notes { get; set; }
+    }
+
+
+
+}
---
+++ Models/Guest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Party.Models
+{
+    public class Guest
+    {
+        public int Id { get; set; } 
+        [Required(ErrorMessage = "Sissesta nimi siia")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Sissesta eaadress siia")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Vale eaadress")]
+        public string Email { get; set; }
+        [RegularExpression(@"\+372[0-9]{8}", ErrorMessage = "Vale number")]//?
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Tee oma valik")]
+        public bool? WillAttend { get; set; }
+
+        public string Notes { get; set; }
+    }
+
+
+
+}
